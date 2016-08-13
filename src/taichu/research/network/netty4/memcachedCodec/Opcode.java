f2c46cdab99fda089562a08cb6416f69b374e5f8
taichu@CHY <275846224@qq.com>
2016-08-14 05:26:42 +0800

    提交tlv研究项目初始代码
    Signed-off-by: taichu@CHY <275846224@qq.com>

---
+++ src/taichu/research/network/netty4/memcachedCodec/Opcode.java
@@ -0,0 +1,8 @@
+package taichu.research.network.netty4.memcachedCodec;
+
+
+public class Opcode {
+    public static final byte GET = 0x00;
+    public static final byte SET = 0x01;
+    public static final byte DELETE = 0x04;
+}
